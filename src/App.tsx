2474618c0fbc3813fa5c4c18e0f617e27a0c56cc
Mateus Alfredo Batista <mateusalfredobatista@gmail.com>
2020-03-20 09:26:58 -0300

    path fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import "./App.css";
 import { Home } from "./components/Home";
 import { PivotTable } from "./components/PivotTable";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <>
       <Home />
       <PivotTable />
-    </div>
+    </>
   );
 };
 
--- src/components/dragndrop/Draggable.tsx
+++ src/components/dragndrop/Draggable.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
 import { useDrag } from "react-dnd";
-import "./Dnd.css";
+import "../../css/Dnd.css";
 
 interface DraggableProps {
   type: string;
